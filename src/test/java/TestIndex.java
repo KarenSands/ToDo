18d92f73d1dbae36550a6878f9df00199cdec275
Emilio Galindo <galindo@Galindo-MacBook-Air.local>
2018-12-04 13:42:58 -0600

    modified:   src/test/java/TestIndex.java

--- src/test/java/TestIndex.java
+++ src/test/java/TestIndex.java
@@ -29,7 +29,7 @@ public class TestIndex {
     @BeforeClass
     public static void setUp() {
         //setear el driver
-        System.setProperty("webdriver.chrome.driver","~/chromedriver");
+        System.setProperty("webdriver.chrome.driver","chromedriver");
         //abrir chrome
         driver = new ChromeDriver(); 
         driver.get(URL);
